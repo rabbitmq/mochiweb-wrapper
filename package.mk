3881f01da411f3e015d67fd4d2b69b55aa25b2ce
David Wragg <david@rabbitmq.com>
2010-12-02 12:32:22 +0000

    Overhaul for bug23568

--- package.mk
+++ package.mk
@@ -7,36 +7,33 @@ CHECKOUT_DIR:=$(PACKAGE_DIR)/$(APP_NAME)-git
 SOURCE_DIR:=$(CHECKOUT_DIR)/src
 INCLUDE_DIR:=$(CHECKOUT_DIR)/src
 
-$(CHECKOUT_DIR)_UPSTREAM_GIT:=$(UPSTREAM_GIT)
-$(CHECKOUT_DIR)_REVISION:=$(REVISION)
-$(CHECKOUT_DIR):
-	git clone $($@_UPSTREAM_GIT) $@
-	(cd $@ && git checkout $($@_REVISION) && patch -p1 < $@-12b3.patch) || (rm -rf $@ && false)
-
-# we run the make_app.escript early just so that we can grab out the mochiweb version
-$(CHECKOUT_DIR)/stamp: | $(CHECKOUT_DIR)
-	rm -f $@
-	cd $(@D) && echo COMMIT_SHORT_HASH:=$$(git log -n 1 --format=format:"%h" HEAD) > $@
-	$(@D)/support/make_app.escript $(@D)/src/mochiweb.app.src $@.tmp "" ""
-	echo $$(cat $@.tmp | grep {vsn | sed -e 's/^.\+{vsn, *\"/MOCHIWEB_VERSION:=/; s/\".*$$//') >> $@
-	rm $@.tmp
-
-$(PACKAGE_DIR)/clean_RM:=$(CHECKOUT_DIR) $(CHECKOUT_DIR)/stamp $(EBIN_DIR)/$(APP_NAME).app
-$(PACKAGE_DIR)/clean::
-	rm -rf $($@_RM)
-
-ifneq "$(strip $(patsubst clean%,,$(patsubst %clean,,$(TESTABLEGOALS))))" ""
-include $(CHECKOUT_DIR)/stamp
+$(eval $(call safe_include,$(PACKAGE_DIR)/version.mk))
 
 VERSION:=$(MOCHIWEB_VERSION)-rmq$(GLOBAL_VERSION)-git$(COMMIT_SHORT_HASH)
 
-$(EBIN_DIR)/$(APP_NAME).app.$(VERSION)_VERSION:=$(VERSION)
-$(EBIN_DIR)/$(APP_NAME).app.$(VERSION)_MAKE_APP:=$(CHECKOUT_DIR)/support/make_app.escript
-$(EBIN_DIR)/$(APP_NAME).app.$(VERSION)_MODULES:=$(patsubst %.erl,%,$(notdir $(wildcard $($(PACKAGE_DIR)_SOURCE_DIR)/*.erl)))
-$(EBIN_DIR)/$(APP_NAME).app.$(VERSION): $(SOURCE_DIR)/$(APP_NAME).app.src | $(EBIN_DIR)
-	$($@_MAKE_APP) $< $@.tmp "" "$($@_MODULES)"
-	sed -e 's/{vsn, *\"[^\"]\+\"/{vsn,\"$($@_VERSION)\"/' < $@.tmp > $@
-	rm $@.tmp
+define package_targets
 
-$(PACKAGE_DIR)_APP:=true
-endif
+$(CHECKOUT_DIR)/.done:
+	rm -rf $(CHECKOUT_DIR)
+	git clone $(UPSTREAM_GIT) $(CHECKOUT_DIR)
+	cd $(CHECKOUT_DIR) && git checkout $(REVISION)
+	patch -d $(CHECKOUT_DIR) -p1 < $(CHECKOUT_DIR)-12b3.patch
+	touch $$@
+
+$(SOURCE_DIR)/$(APP_NAME).app.src: $(CHECKOUT_DIR)/.done
+
+$(CHECKOUT_DIR)/$(APP_NAME).app.orig: $(SOURCE_DIR)/$(APP_NAME).app.src
+	$(CHECKOUT_DIR)/support/make_app.escript $$< $$@ "" "`cd $(SOURCE_DIR) && echo *.erl | sed 's|\.erl||g'`"
+
+$(PACKAGE_DIR)/version.mk: $(CHECKOUT_DIR)/$(APP_NAME).app.orig
+	echo COMMIT_SHORT_HASH:=`git --git-dir=$(CHECKOUT_DIR)/.git log -n 1 --format=format:"%h" HEAD` >$$@
+	sed -n -e 's|^.*{vsn, *"\([^"]*\)".*$$$$|MOCHIWEB_VERSION:=\1|p' <$$< >>$$@
+
+$(EBIN_DIR)/$(APP_NAME).app: $(CHECKOUT_DIR)/$(APP_NAME).app.orig $(PACKAGE_DIR)/version.mk
+	@mkdir -p $$(@D)
+	sed -e 's/{vsn, *\"[^\"]\+\"/{vsn,\"$(VERSION)\"/' <$$< >$$@
+
+$(PACKAGE_DIR)+clean::
+	rm -rf $(CHECKOUT_DIR) $(EBIN_DIR)/$(APP_NAME).app $(PACKAGE_DIR)/version.mk
+
+endef
