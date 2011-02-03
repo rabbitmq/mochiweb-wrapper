e362ab0b3eaff999219f3fadefcb9a25f1fad824
David Wragg <david@rabbitmq.com>
2011-02-03 11:54:11 +0000

    Unify wrapper and non-wrapper package app file generation

--- package.mk
+++ package.mk
@@ -8,16 +8,16 @@ WRAPPER_PATCHES:=mochiweb-12b3.patch 10-crypto.patch
 # internal.hrl is used by webmachine
 UPSTREAM_INCLUDE_DIRS+=$(CLONE_DIR)/src
 
-UPSTREAM_APP_FILE:=$(CLONE_DIR)/$(APP_NAME).app
+ORIGINAL_APP_FILE:=$(CLONE_DIR)/$(APP_NAME).app
 
 define package_targets
 
 $(CLONE_DIR)/src/$(APP_NAME).app.src: $(CLONE_DIR)/.done
 
-$(UPSTREAM_APP_FILE): $(CLONE_DIR)/src/$(APP_NAME).app.src
+$(ORIGINAL_APP_FILE): $(CLONE_DIR)/src/$(APP_NAME).app.src
 	$(CLONE_DIR)/support/make_app.escript $$< $$@ "" "`cd $(CLONE_DIR)/src && echo *.erl | sed 's|\.erl||g'`"
 
 $(PACKAGE_DIR)+clean::
-	rm -rf $(UPSTREAM_APP_FILE)
+	rm -rf $(ORIGINAL_APP_FILE)
 
 endef
