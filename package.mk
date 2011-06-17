2eece138be719553eca085674a3598e84adf700b
David Wragg <david@rabbitmq.com>
2011-06-18 00:46:23 +0100

    Revert bug24186

--- package.mk
+++ package.mk
@@ -8,13 +8,15 @@ WRAPPER_PATCHES:=mochiweb-12b3.patch 10-crypto.patch
 # internal.hrl is used by webmachine
 UPSTREAM_INCLUDE_DIRS+=$(CLONE_DIR)/src
 
+ORIGINAL_APP_FILE:=$(CLONE_DIR)/$(APP_NAME).app
+DO_NOT_GENERATE_APP_FILE=true
+
 define package_rules
 
 $(CLONE_DIR)/src/$(APP_NAME).app.src: $(CLONE_DIR)/.done
 
 $(ORIGINAL_APP_FILE): $(CLONE_DIR)/src/$(APP_NAME).app.src
-	@mkdir -p $$(@D)
-	$(CLONE_DIR)/support/make_app.escript $$< $$@ "" ""
+	$(CLONE_DIR)/support/make_app.escript $$< $$@ "" "`cd $(CLONE_DIR)/src && echo *.erl | sed 's|\.erl||g'`"
 
 $(PACKAGE_DIR)+clean::
 	rm -rf $(ORIGINAL_APP_FILE)
