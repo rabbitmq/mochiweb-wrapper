5b36e05d775550b115a6ce3a1222f4545f3c885d
Simon MacMullen <simon@rabbitmq.com>
2011-09-23 12:41:55 +0100

    Bump mochiweb request size limit from 1MB to 100MB. Patch from Gavin Roy.

--- package.mk
+++ package.mk
@@ -3,7 +3,7 @@ APP_NAME:=mochiweb
 UPSTREAM_GIT:=http://github.com/mochi/mochiweb.git
 UPSTREAM_REVISION:=9a53dbd7b2c52eb5b9d4
 RETAIN_ORIGINAL_VERSION:=true
-WRAPPER_PATCHES:=mochiweb-12b3.patch 10-crypto.patch
+WRAPPER_PATCHES:=mochiweb-12b3.patch 10-crypto.patch 20-MAX_RECV_BODY.patch
 
 # internal.hrl is used by webmachine
 UPSTREAM_INCLUDE_DIRS+=$(CLONE_DIR)/src
