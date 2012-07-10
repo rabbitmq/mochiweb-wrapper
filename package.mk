cd8ddfa8f643e8e5033c2f5215b625acb4a98caa
Simon MacMullen <simon@rabbitmq.com>
2012-07-10 12:21:38 +0100

    Update to latest Mochiweb. New patch to build on R12B-5 (basically just strip out all the specs), and update the patch to bump MAX_RECV_BODY. Lose the patch to not require the crypto app, since now Mochiweb requires a bunch more apps - ssl in particular, but also compiler and syntax_tools. We need to think about this.

--- package.mk
+++ package.mk
@@ -1,9 +1,9 @@
 APP_NAME:=mochiweb
 
 UPSTREAM_GIT:=http://github.com/mochi/mochiweb.git
-UPSTREAM_REVISION:=9a53dbd7b2c52eb5b9d4
+UPSTREAM_REVISION:=d541e9a0f36c00dcadc2e589f20e47fbf46fc76f
 RETAIN_ORIGINAL_VERSION:=true
-WRAPPER_PATCHES:=mochiweb-12b3.patch 10-crypto.patch 20-MAX_RECV_BODY.patch
+WRAPPER_PATCHES:=10-build-on-R12B-5.patch 20-MAX_RECV_BODY.patch
 
 # internal.hrl is used by webmachine
 UPSTREAM_INCLUDE_DIRS+=$(CLONE_DIR)/src
@@ -16,7 +16,7 @@ define package_rules
 $(CLONE_DIR)/src/$(APP_NAME).app.src: $(CLONE_DIR)/.done
 
 $(ORIGINAL_APP_FILE): $(CLONE_DIR)/src/$(APP_NAME).app.src
-	$(CLONE_DIR)/support/make_app.escript $$< $$@ "" "`cd $(CLONE_DIR)/src && echo *.erl | sed 's|\.erl||g'`"
+	cp $(CLONE_DIR)/src/$(APP_NAME).app.src $(ORIGINAL_APP_FILE)
 
 $(PACKAGE_DIR)+clean::
 	rm -rf $(ORIGINAL_APP_FILE)
