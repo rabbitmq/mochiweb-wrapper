125091968019671af17739fc12d8f29b6ff2a7a4
Simon MacMullen <simon@rabbitmq.com>
2013-07-22 16:43:22 +0100

    Freshen Mochiweb

--- package.mk
+++ package.mk
@@ -1,14 +1,13 @@
 APP_NAME:=mochiweb
 
 UPSTREAM_GIT:=http://github.com/rabbitmq/mochiweb.git
-UPSTREAM_REVISION:=d541e9a0f36c00dcadc2e589f20e47fbf46fc76f
+UPSTREAM_REVISION:=680dba8a8a0dd8ee18d03bf814cfb2340bf3bbff
 RETAIN_ORIGINAL_VERSION:=true
 WRAPPER_PATCHES:=10-build-on-R12B-5.patch \
 		 20-MAX_RECV_BODY.patch \
 		 30-remove-crypto-ssl-dependencies.patch \
 		 40-remove-compiler-syntax_tools-dependencies.patch \
-		 50-remove-json.patch \
-		 60-parameterised-modules-r16a.patch
+		 50-remove-json.patch
 
 # internal.hrl is used by webmachine
 UPSTREAM_INCLUDE_DIRS+=$(CLONE_DIR)/src
